6e86362b98cfaa2475294de672f0199d2c88e11f
Shanate <redeyes5240@gmail.com>
2023-07-03 15:53:51 +0900

    SR ratch example

---
+++ HDLex/SRratch/SRratch.v
@@ -0,0 +1,31 @@
+module SR_latch(Q, Qbar, Sbar, Rbar);
+
+output Q, Qbar;
+input Sbar, Rbar;
+
+nand n1(Q, Sbar, Qbar);
+nand n2(Qbar, Rbar, Q);
+endmodule
+
+module Top;
+
+wire q, qbar;
+reg set, reset;
+
+SR_latch m1(q, qbar, ~set, ~reset);
+initial
+begin
+	$monitor($time, "set = %b, reset= %b, q= %b\n", set, reset, q);
+	set =0; reset =0;
+	#5 reset = 1;
+	#5 reset =0;
+	#5 set=1;
+	#100 $finish;
+end
+initial begin
+	$dumpfile("tesetbench.vcd");
+	$dumpvars;
+end
+
+
+endmodule
