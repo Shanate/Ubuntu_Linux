5d935b4312b1f402b40590c7c280c8b3add90e8b
Shanate <redeyes5240@gmail.com>
2023-07-10 11:59:04 +0900

    cla project

--- myproject/cla/cla.v
+++ myproject/cla/cla.v
@@ -20,7 +20,7 @@ output	S, C
 
 
 	wire	S1,S2,C1,C2;
-//	wire	S, C;
+
 // instantiation
 half_adder	ha1(
 		.A(A),
@@ -43,45 +43,46 @@ endmodule
 // 4bit CLA module
 
 module CLA4(
-input	wire	[3:0]	P,
-input	wire	[3:0]	G,
-input	wire		Cin,
-output	wire	[3:0]	,
-output	wire		Cout
+input	wire	[3:0]	P,   // Propagation = A(i) ^ B(i)
+input	wire	[3:0]	G,   // Generation = A(i) & B(i)
+input	wire		Cin, // Initial Carry In, it means C[0]
+output	wire	[4:1]	C,
+output	wire		PG,  // PG = P(0)&P(1)&P(2)&P(3)
+output	wire		GG   // GG = G(3)+G(2)P(3)+G(1)P(3)P(2)+G(0)P(3)P(2)P(1)
 );
 
-	wire	[4:1]	C;
+//	wire	[4:1]	C; // C(i+1) = G(i) + P(i)C(i)
 
 full_adder FA0(
 		.A(A[0]),
 		.B(B[0]),
 		.Cin(Cin),
 		.S(S[0]),
-		.C(C[0])
+		.C(C[1])
 		);
 
 full_adder FA1(
 		.A(A[1]),
 		.B(B[1]),
-		.Cin(C[0]),
+		.Cin(C[1]),
 		.S(S[1]),
-		.C(C[1])
+		.C(C[2])
 		);
 
 full_adder FA2(
 		.A(A[2]),
 		.B(B[2]),
-		.Cin(C[1]),
+		.Cin(C[2]),
 		.S(S[2]),
-		.C(C[2])
+		.C(C[3])
 		);
 
 full_adder FA3(
 		.A(A[3]),
 		.B(B[3]),
-		.Cin(C[2]),
+		.Cin(C[3]),
 		.S(S[3]),
-		.C(C[3])
+		.C(C[4])
 		);
 
 
@@ -90,21 +91,35 @@ endmodule
 ///////////////////////////////////
 // 64bit Carry Look Ahead Adder Module
 
-module CLA64(
-input	wire	[63:0]	A,
-input	wire	[63:0]	B,
-input	wire		Cin,
-input	wire	[63:0]	S,
+module CLA16(
+input	wire	[15:0]	A,
+input	wire	[15:0]	B,
+input	wire	[,
+input	wire	[15:0]	S,
 output	wire		Cout	
 
 );
 
-	wire	[16:1]	C;
+//test			wire	[16:1]	C;
+
+CLA4	cla1(
+	.P(P[0]),
+	.G(G[0]),
+	.C(C[0]),
+	.PG(),
+	.GG()
+	);
 
 
-
+input	wire	[3:0]	P,   // Propagation = A(i) ^ B(i)
+input	wire	[3:0]	G,   // Generation = A(i) & B(i)
+input	wire		Cin, // Initial Carry In, it means C[0]
+output	wire	[4:1]	C,
+output	wire		PG,  // PG = P(0)&P(1)&P(2)&P(3)
+output	wire		GG   // GG = G(3)+G(2)P(3)+G(1)P(3)P(2)+G(0)P(3)P(2)P(1)
+);
 
 // G : Generation = A * B
-// P : Propagation = A + B
+// P : Propagation = A ^ B
 // C(i+1) = A(i)B(i) + {A(i)+B(i)}C(i) = G(i) + P(i)C(i)
 endmodule
